bdd545fc2d3f2d3b4e88574f65374f1d7f62d726
ItsIvanPsk <ivanfigueredo.et@gmail.com>
2024-07-24 07:34:27 +0200

    User Login Controller

--- src/routes/hospital.js
+++ src/routes/hospital.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createSession, endSession, playerCheckpoint, sessionList } = require('../controllers/hospitalController');
+const { createSession, endSession, playerCheckpoint, sessionList, playerData } = require('../controllers/hospitalController');
 
 router.post('/create_session', createSession);
 router.post('/end_session', endSession);
 router.post('/player_checkpoint', playerCheckpoint);
+router.post('/get_player_data', playerData);
 router.post('/get_sessions', sessionList);
 
 module.exports = router;
